0ed7b5873b36d41fd3f4ef543ee5f159d4308113
Abdul Ahad <ahadbutt.pk@gmail.com>
2021-05-25 12:00:57 +0500

    Finished a smaller prototype

--- server.py
+++ server.py
@@ -1,4 +1,19 @@
-import socket 
- 
-ip = socket.gethostbyname('www.google.com')
-print(ip)
+import socket
+
+s = socket.socket()
+port = 12345
+
+s.bind(('', port))
+print("Socket binded to %s" %(port))
+
+s.listen(5)
+print('Socket is listening')
+
+
+while True:
+    c, addr = s.accept()
+    print("Got connection from ", addr)
+
+    c.send('Thank you for connecting'.encode())
+
+    c.close()
